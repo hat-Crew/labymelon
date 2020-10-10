8fcf38cf7da8a04c61c40b725e2997ca50fda106
Nielk Nilke <antoineklein2000@gmail.com>
2020-10-10 13:39:46 +0100

    on obtient des coordonées aléatoires à partir d'une matrice

--- index.js
+++ index.js
@@ -3,8 +3,38 @@ const app = express();
 
 
 
-app.get('/test', function(req, res){
-	res.send('ceci est un test');
+var matrice = [
+    [1, 1, 1, 1],
+    [1, 0, 0, 1],
+    [1, 0, 0, 1],
+    [1, 1, 1, 1]
+]
+var available_points = Array()
+var x = 0
+var y = 0
+var coinIndex;
+matrice.forEach(function(line) {
+    x = 0
+    line.forEach(function(ele) {
+        if (ele === 0) {
+            available_points.push([x, y])
+        }
+        x++
+    })
+    y++
+})
+coinIndex = Math.round(Math.random() * available_points.length)
+
+app.get('/newCoin', function(req, res) {
+    coinIndex = Math.round(Math.random() * (available_points.length - 1))
+    return res.json({ 'coin': { 'x': available_points[coinIndex][0], 'y': available_points[coinIndex][1] } });
+})
+app.get('/getCoin', function(req, res) {
+    return res.json({ 'coin': { 'index': available_points.length, 'x': available_points[coinIndex][0], 'y': available_points[coinIndex][1] } });
+})
+
+app.get('/test', function(req, res) {
+    res.send('ceci est un test');
 });
 
 app.listen(3000);
