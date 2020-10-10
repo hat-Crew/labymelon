d36e0405307fceb79a0ecae14630b1f3c802748c
Firologame <erwan1207@gmail.com>
2020-10-10 16:50:04 +0200

    commentaire de push

--- index.js
+++ index.js
@@ -9,8 +9,23 @@ git push
 
 */
 
-app.get('/test', function(req, res){
-	res.send('ceci est un test');
+app.post('/move', function(req, res){ // requete post /move?token=thetoken&vx=newx&vy=newy
+	if(check_tocken(req.query.token)){
+		if(map[req.quer.vx][req.quer.vy] == 1)
+			res.send('you can\'t move through walls !');
+		else {
+			movePlayer(req.query.token, req.query.vx, req.query.vy)
+			.then(function(){
+				res.send('moved');
+			});
+		}
+	}
+	else
+		res.send('you need a token to request !');
 });
+
+function movePlayer(token, newX, newY) {
+	getUserByToken(token).position = { x:newX, y:newY };
+}
 
 app.listen(3000);
