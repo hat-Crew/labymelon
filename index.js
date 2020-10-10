2054e885c57512dac1c266b1236f515e631dc6d4
Firologame <erwan1207@gmail.com>
2020-10-10 17:49:51 +0200

    anti diagonal moves

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
 var express = require('express');
 const app = express();
 
-// requete post /move?token=thetoken&vx=newx&vy=newy
-app.post('/move', function(req, res){
+// check si le joueur a avancé d'une case, s'il ne se déplace pas en diagonales et s'il ne va pas sur un mur
+function movePlayer(token, vx, vy) {
+	let currentPos = getUserByToken(token).position;
+	console.log(currentPos.x + vx == currentPos.x+1);
+	if(map[currentPos.x+vx][currentPos.y+vy] == 0) { // check si on est sur un mur ou pas
+		if(currentPos.x + vx == currentPos.x+1 || currentPos.x + vx == currentPos.x-1 || currentPos.x + vx == currentPos.x) {
+			if(currentPos.y + vy == currentPos.y+1 || currentPos.y + vy == currentPos.y-1 || currentPos.y + vy == currentPos.y) {
+				if(vx+vy < 2 && vx+vy > -2) // évite de se déplacer en diagonales
+					return true;
+			}
+		}
+	}
+	return false;
+}
+
+
+// requete post /move?token=thetoken&vx=vx&vy=vy
+app.get('/move', function(req, res){
 	if(check_token(req.query.token)){
-		if(map[req.quer.vx][req.quer.vy] == 1)
-			res.send('you can\'t move through walls');
+		if(movePlayer(req.query.token, parseInt(req.query.vx), parseInt(req.query.vy))) {
+				getUserByToken(req.query.token).position.x += parseInt(req.query.vx); // on update la position du joueur
+				getUserByToken(req.query.token).position.y += parseInt(req.query.vy);
+				res.send('ok');
+		}
 		else {
-			if(movePlayer(req.query.token, req.query.vx, req.query.vy)) {
-				getUserByToken(token).position = { x:newX, y:newY }; // on update la position du joueur
-				res.send('successfully moved');
-			}
-			else {
-				res.send('you can\'t teleport');
-			}
-			
+				res.send('you can\'t move');
 		}
 	}
 	else
 		res.send('you need to specify a valid token');
 });
-
-// check si le joueur a avancé d'une case et pas plus
-function movePlayer(token, newX, newY) {
-	let currentPos = getUserByToken(token).position;
-	if(currentPos.x + newX == currentPos.x+1 || currentPos.x + newX == currentPos.x-1 || currentPos.x + newX == currentPos.x) {
-		if(currentPos.y + newY == currentPos.y+1 || currentPos.y + newY == currentPos.y-1 || currentPos.y + newY == currentPos.y)
-			return false;
-	}
-	return true;
-}
 
 app.listen(3000);
