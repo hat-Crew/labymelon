249297a77ec923d79479f43b2f7970a9908e8b0c
Firologame <erwan1207@gmail.com>
2020-10-10 17:06:17 +0200

    anti teleport added

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
 var express = require('express');
 const app = express();
 
-/*
-
-git add -A
-git commit -m 'commentaire de push'
-git push
-
-*/
-
-app.post('/move', function(req, res){ // requete post /move?token=thetoken&vx=newx&vy=newy
-	if(check_tocken(req.query.token)){
+// requete post /move?token=thetoken&vx=newx&vy=newy
+app.post('/move', function(req, res){
+	if(check_token(req.query.token)){
 		if(map[req.quer.vx][req.quer.vy] == 1)
-			res.send('you can\'t move through walls !');
+			res.send('you can\'t move through walls');
 		else {
-			movePlayer(req.query.token, req.query.vx, req.query.vy)
-			.then(function(){
-				res.send('moved');
-			});
+			if(movePlayer(req.query.token, req.query.vx, req.query.vy)) {
+				getUserByToken(token).position = { x:newX, y:newY }; // on update la position du joueur
+				res.send('successfully moved');
+			}
+			else {
+				res.send('you can\'t teleport');
+			}
+			
 		}
 	}
 	else
-		res.send('you need a token to request !');
+		res.send('you need to specify a valid token');
 });
 
+// check si le joueur a avancé d'une case et pas plus
 function movePlayer(token, newX, newY) {
-	getUserByToken(token).position = { x:newX, y:newY };
+	let currentPos = getUserByToken(token).position;
+	if(currentPos.x + newX == currentPos.x+1 || currentPos.x + newX == currentPos.x-1 || currentPos.x + newX == currentPos.x) {
+		if(currentPos.y + newY == currentPos.y+1 || currentPos.y + newY == currentPos.y-1 || currentPos.y + newY == currentPos.y)
+			return false;
+	}
+	return true;
 }
 
 app.listen(3000);
