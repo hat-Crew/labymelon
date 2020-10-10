1739d6fad1c9323f060ba5be76e21987bd1621a4
Thibault (Dinnox) <thibault-dernis@hotmail.fr>
2020-10-10 15:15:36 +0200

    Added token generator & '/auth' route

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 var express = require('express');
+var md5 = require('md5');
 const app = express();
 
-
+let user_tokens = [];
 
 app.get('/test', function(req, res){
 	res.send('ceci est un test');
 });
 
 app.get('/auth', function(req, res) {
-	res.send('auth');
+	res.send(generateToken());
 });
 
 app.listen(3000);
+
+
+function generateToken() {
+	let token = md5((Math.random() * 10 + '' + Date.now()).slice(2) + '' + Date.now());
+	user_tokens.push(token);
+	return token;
+
+}
