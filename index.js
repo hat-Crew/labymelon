e7710d93ffcd24fbe1647a6da6f5e3dbe9d4ed7b
Gqsp <71782138+Gqsp@users.noreply.github.com>
2020-10-11 15:11:53 +0200

    code commentee, petite opti sur le clean map

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ let path;
 let checkFor;
 let found;
 
-function generateEmptyMap(map){
+function generateRawMap(map){
     for (let i = 0; i < ms.x; i++){
         map.push([])
         for (let j = 0; j < ms.y; j++){
-            map[i][j] = (i === 0 || j === 0 || i === ms.x || j === ms.y || !(i % 4) || !(j % 4)) ? 1 : 0;
+            map[i][j] = (i === 0 || j === 0 || i === ms.x || j === ms.y || !(i % 4) || !(j % 4)) ? 1 : 0; // sets the borders as wall, and creates a grid in the middle every 4 blocks
         }
     }
     return map;
@@ -50,11 +50,11 @@ function generatePath(map){
                 }
 
                 switch(nbOfPaths){ //depending on the numbers of already existent paths, create a certain amount of new ones
-                    case 0: nbNewPaths = 2 + Math.floor(Math.random() * 3); break;
-                    case 1: nbNewPaths = 1 + Math.floor(Math.random() * 3); break;
-                    case 2: nbNewPaths = Math.floor(Math.random() * 2); break;
-                    case 3: nbNewPaths = Math.floor(Math.random() * 2); break;
-                    case 4: nbNewPaths = 0; break;
+                    case 0: nbNewPaths = 2 + Math.floor(Math.random() * 3); break; // (2 - 4 paths)
+                    case 1: nbNewPaths = 1 + Math.floor(Math.random() * 3); break; // (1 - 3 paths)
+                    case 2: nbNewPaths = Math.floor(Math.random() * 2); break; // (0 - 1 path)
+                    case 3: nbNewPaths = Math.floor(Math.random() * 2); break; // (0 - 1 path)
+                    case 4: nbNewPaths = 0; break; 
                 }
 
                 for(let m = 0; m < nbNewPaths; m++){ // For each new paths 
@@ -62,10 +62,10 @@ function generatePath(map){
                     path = unusedPath[index]; // find the direction linked to the random index
                     
                     switch(path){ //remove a wall in the corresponding direction
-                        case 0: map[k - 1][l] = 0; break;
-                        case 1: map[k][l - 1] = 0; break;
-                        case 2: map[k + 1][l] = 0; break;
-                        case 3: map[k][l + 1] = 0; break;
+                        case 0: map[k - 1][l] = 0; break; //top
+                        case 1: map[k][l - 1] = 0; break; //left
+                        case 2: map[k + 1][l] = 0; break; //bottom
+                        case 3: map[k][l + 1] = 0; break; //right
                     }
                 }
             }
@@ -76,35 +76,32 @@ function generatePath(map){
 
 function cleanMap(map){
 
-    for (i = 1; i < ms.x - 1; i++){
-        for (j = 1; j < ms.y - 1; j++){
-            checkFor = (map[i][j]);
+    for (i = 1; i < ms.x - 1; i++){ //cycling through all slots
+        for (j = 1; j < ms.y - 1; j++){ 
+            checkFor = (map[i][j]); // we will look for same element as the current element in its surrounding
             found = false;
 
-            found = (map[i - 1][j] == checkFor) ? true : found;
-            found = (map[i - 1][j + 1] == checkFor) ? true : found;
-            found = (map[i - 1][j - 1] == checkFor) ? true : found;
+            for (k = -1; k <= 1 && !found; k ++){
+                for (l = -1; l <= 1 && !found; l ++){
+                    if (!(k == 0 && l == 0) && map[i + k][j + l] == checkFor){ //if the same type of element as the middle one, we will set the value found as true
+                        found = true; //when it is found, we will stop cycling through the surroundings
+                    }
+                }
+            }
 
-            found = (map[i + 1][j] == checkFor) ? true : found;
-            found = (map[i + 1][j - 1] == checkFor) ? true : found;
-            found = (map[i + 1][j + 1] == checkFor) ? true : found;
-            
-            found = (map[i][j - 1] == checkFor) ? true : found;
-            found = (map[i][j + 1] == checkFor) ? true : found;
-
-            if (!found){
-                map[i][j] = (map[i][j]) ? 0 : 1;
+            if (!found){ //if no element has been found
+                map[i][j] = (map[i][j]) ? 0 : 1; //invert the middle element, to match its surroundings
             }
         }
     }
     return map;
 }
 
-function generateRandomMap(){
-    map = [];
-    map = generateEmptyMap(map);
-    map = generatePath(map)
-    map = cleanMap(map);
+function generateRandomMap(){ 
+    map = []; //empty map
+    map = generateRawMap(map); //this will generate a map with all walls filled
+    map = generatePath(map); //this will generate all paths
+    map = cleanMap(map); //this will clean singular elements
     return map;
 };
 
